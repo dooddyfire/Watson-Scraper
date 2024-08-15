566c6f8910ed44d31f08e086b86969d075ac8fa6
Supapong Sakulkoo <49072725+dooddyfire@users.noreply.github.com>
2024-08-15 17:00:37 +0700

    Update appx.py

--- appx.py
+++ appx.py
@@ -55,7 +55,7 @@ price_discount_lis = []
 prod_name_lis = []
 member_price_lis = []
 item_link_lis = []
-
+pro2_lis = []
 driver = Driver(uc=True)
 
 
@@ -72,27 +72,39 @@ for page in range(start,end+1):
     lis = [ k for k in driver.find_elements(By.CSS_SELECTOR,'div.productContainer')]
 
 
+    c = 1
     for i in lis:
+        driver.get(i)
+
+        time.sleep(5)
+        try:
+               btn_cookie = driver.find_element(By.CSS_SELECTOR,'btn#onetrust-accept-btn-handler')
+               btn_cookie.click()
+        except:
+               pass
+
+        soup = BeautifulSoup(driver.page_source,'html.parser')
+
 
         try:
-                prod_name = i.find_element(By.CSS_SELECTOR,'h2').text
-            
+                #prod_name = i.find_element(By.CSS_SELECTOR,'div.product-name').text
+                prod_name = i.split("/")[-3]
         except: 
                 prod_name = '-'
         print('Name : ',prod_name)
+        prod_name_lis.append(prod_name)
 
-
-        try:
-                url_item= i.find_element(By.CSS_SELECTOR,'a.ClickSearchResultEvent_Class').get_attribute('href')
+        
+        url_item= i
             
-        except: 
-                purl_item = '-'
+
         print('Link: ',url_item)
         item_link_lis.append(url_item)
 
 
         try: 
-                promotion = "\n".join( list(set([k.text.strip() for k in driver.find_elements(By.CSS_SELECTOR,'div.productHighlight')])) )
+                #promotion = "\n".join( list(set([k.text.strip() for k in driver.find_elements(By.CSS_SELECTOR,'div.coupon')])) )
+                promotion = "\n".join( list(set([k.text.strip() for k in soup.find_all('e2-ecoupon-item')])) )
 
         except:
                 promotion = '-'
@@ -101,7 +113,8 @@ for page in range(start,end+1):
         pro_item_lis.append(promotion)
 
         try:
-                price = i.find_element(By.CSS_SELECTOR,'div.productOriginalPrice ').text 
+                #price = i.find_element(By.CSS_SELECTOR,'span.retail-price').text
+                price = soup.find('span',{'class':'retail-price'}).text
                 print(price)
         except: 
                 price = '-'
@@ -110,8 +123,9 @@ for page in range(start,end+1):
         buy_price_lis.append(price)
 
         try:
-                dc_price = i.find_element(By.CSS_SELECTOR,'div.formatted-value').text 
-            
+                #dc_price = i.find_element(By.CSS_SELECTOR,'span.price').text 
+                dc_price = soup.find('span',{'class':'price'}).text
+
         except:
 
                 dc_price = '-'
@@ -119,7 +133,7 @@ for page in range(start,end+1):
         price_discount_lis.append(dc_price)
 
         try:
-                member_price = i.find_element(By.CSS_SELECTOR,'div.memberPrice-content').text.strip()
+                member_price = soup.find_all('span',{'class':'price'})[1].text
 
         except: 
                 member_price = '-'
@@ -128,15 +142,24 @@ for page in range(start,end+1):
         member_price_lis.append(member_price)
 
         try: 
-                bought = i.find_element(By.CSS_SELECTOR,'div.social-proof-box').text.strip()
-            
+                #bought = i.find_element(By.CSS_SELECTOR,'div.social-proof').text.strip()
+                bought = soup.find('div',{'class':'social-proof'}).text.strip()
+
         except:
                 bought = '-'
 
         print('Total Sell : ',bought)
         bought_lis.append(bought)
 
+        try: 
+               pro2 = "\n".join([x.text for x in soup.find_all('div',{'class':'promotion-group'})])
+               
+        except:
+               pro2 = '-' 
+        print('promotion 2 : ',pro2)
+        pro2_lis.append(pro2)
         print()
+        c = c + 1
 
 df = pd.DataFrame()
 df['Name'] = prod_name_lis 
@@ -145,6 +168,7 @@ df['Discount Pirce'] = price_discount_lis
 df['Member Price'] = member_price_lis 
 df['ขายไปแแล้ว'] = bought_lis 
 df['Promotion'] = pro_item_lis 
+df['Promotion 2'] = pro2_lis
 df['Link'] = item_link_lis
 df.to_excel(filename+'.xlsx')
 
